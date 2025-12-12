e00097f2245c0d146f37e3bd294c3a9b23fe4d51
Max Koste <60856309+maxkoste@users.noreply.github.com>
2025-12-12 16:55:43 +0100

    added TODOs for the game logic

--- src/game/blackjack.py
+++ src/game/blackjack.py
@@ -1,6 +1,11 @@
+# TODO: Make API calls to the decks of cards API
+# TODO: Write the game logic for the dealer and have the dealer draw cards based on blackjack rules
+# TODO: Save points and keep track of both dealer and players hands
+# TODO: Update and keep track of the total amount of cash based on loss or win
+
 BASE_URL = "deck of cards url here!"
 
 
 def hit():
-    print("I was Called!")
-    return "SUCESS"
+    print("Called from blackjack.py")
+    return "Proof of concept SUCESSFULL"
